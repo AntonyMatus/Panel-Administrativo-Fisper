93648cdeed0d719d475fbc29bda8c4a09660bd7e
tony_ <antonyrebolledo@gmail.com>
2022-08-15 17:19:56 -0500

    intregracion del panel administrativo Lexa

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <title>Fisper - Panel Admin Dashboard</title>
+        <meta content="Admin Dashboard" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <link rel="stylesheet" href="../plugins/morris/morris.css">
+
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/metismenu.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+        <?php include('includes/navbar-top.php'); ?>
+
+        <?php include('includes/sidebar.php'); ?>
+
+        <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container-fluid">
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+include('includes/header.php');
+
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="page-title-box">
+            <h4 class="page-title">FISPER</h4>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active">
+                    Welcome to Fisper Dashboard
+                </li>
+            </ol>
+           
+        </div>
+    </div>
+</div>
+<!-- end row -->
+
+<?php 
+include('includes/footer.php');
+include('includes/scripts.php');
+
+?>
